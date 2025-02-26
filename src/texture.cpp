0a3876d7627e68567d083ee7b35bb5ea45dfb39f
Ann-Sophie St-Amand <anso.st.amand02@gmail.com>
2025-02-26 14:15:08 -0500

    constructeur texture

--- src/texture.cpp
+++ src/texture.cpp
@@ -14,8 +14,26 @@ Texture2D::Texture2D(const char* path)
     if (data == NULL)
         std::cout << "Error loading texture \"" << path << "\": " << stbi_failure_reason() << std::endl;
 
-    // TODO - Chargement de la texture, attention au format des pixels de l'image!
+    glGenTextures(1, &m_id);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, m_id);
 
+    switch (nChannels) {
+        case 1:
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, data);
+            break;
+        case 2:
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RG, width, height, 0, GL_RG, GL_UNSIGNED_BYTE, data);
+            break;
+        case 3:
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            break;
+        case 4:
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+            break;
+        default:
+            break;
+    }
     stbi_image_free(data);
 }
 
