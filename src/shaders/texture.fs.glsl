7184fc5095126d082ac36a8824823543b8973615
Ann-Sophie St-Amand <anso.st.amand02@gmail.com>
2025-02-27 15:03:14 -0500

    fix shader texture

--- src/shaders/texture.fs.glsl
+++ src/shaders/texture.fs.glsl
@@ -3,10 +3,10 @@
 in vec2 vertexCoords;
 out vec4 FragColor;
 
-uniform sampler2D texture;
+uniform sampler2D tex;
 
 void main()
 {
-    vec4 texColor = texture(texture, vertexCoords);
+    vec4 texColor = texture(tex, vertexCoords);
     FragColor = texColor;
 }
